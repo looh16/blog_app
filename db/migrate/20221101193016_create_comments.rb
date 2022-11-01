bdf76d22a3d97c5fbe673bf61f6bad2cbc494f3b
looh16 <langacustodio@gmail.com>
2022-11-01 21:56:06 +0200

    Update migrations and and foreign keys

--- db/migrate/20221101193016_create_comments.rb
+++ db/migrate/20221101193016_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :authorId
-      t.integer :postId
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
       t.string :text
 
       t.timestamps
--- db/migrate/20221101194105_create_likes.rb
+++ db/migrate/20221101194105_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :authorId
-      t.integer :postId
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
