2e37161e232abe22c0acb6258f11f12811ac109c
looh16 <langacustodio@gmail.com>
2022-11-03 20:46:16 +0200

    fix errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-
   validates :name, presence: true
-  validates :posts_counter, :numericality => { greater_than_or_equal_to: 0, only_integer: true}
-
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def last_posts
     posts.order(created_at: :desc).limit(3)
