f5e141f9f56cfce4471b751275fca71470dbe55a
looh16 <langacustodio@gmail.com>
2022-11-07 19:24:34 +0200

    Add  controler, view and route for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "users" => "users#index"
-  get 'users/:id' => 'users#show'
+  get "users/:id" => "users#show"
+  get "users/:id/posts" => "posts#index"
+  get "users/:id/posts/:id" => "posts#show"
 end
