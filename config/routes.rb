a89e45be57f6ff4a6fa14a74de13b5d53d6c8fdc
looh16 <langacustodio@gmail.com>
2022-11-08 13:07:13 +0200

    Update routers file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  get "users" => "users#index"
-  get "users/:id" => "users#show"
+  #get "users" => "users#index"
+  #get "users/:id" => "users#show"
+  resources :users, only: [:show, :index]
+
   get "users/:id/posts" => "posts#index"
-  get "users/:id/posts/:id" => "posts#show"
+ # resources :users do
+ #   resources :posts, only: [:show]
+ # end
+
+ # get "users/:id/posts/:id" => "posts#show"
+  resources :users do
+    resources :posts
+  end
+
+
+
 end
