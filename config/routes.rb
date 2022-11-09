f0b284968402efe04fc830230827c8f1e0627073
looh16 <langacustodio@gmail.com>
2022-11-09 20:45:07 +0200

    Add default router

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root "users#index"
+  
   get "users" => "users#index"
   get "users/:id" => "users#show", as: "user"
   get "users/:id/posts" => "posts#index", as: "user_post"
