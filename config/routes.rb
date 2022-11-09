0be9090b1dab4d2452cb400d1433f97e3d044f2e
looh16 <langacustodio@gmail.com>
2022-11-09 20:38:14 +0200

    Add index ad show views to user posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "users" => "users#index"
   get "users/:id" => "users#show", as: "user"
   get "users/:id/posts" => "posts#index", as: "user_post"
-  get "users/:id/posts/:id" => "posts#show"
+  get "users/:id/posts/:id" => "posts#show", as: "post_detail"
 end
