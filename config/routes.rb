ad1cdfeac6767786a45b444f9b5643d2a9251420
looh16 <langacustodio@gmail.com>
2022-11-10 14:35:23 +0200

    Update routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   root "users#index"
   
-  get "users" => "users#index"
-  get "users/:id" => "users#show", as: "user"
-  get "users/:id/posts" => "posts#index", as: "user_post"
-  get "users/:id/posts/:id" => "posts#show", as: "post_detail"
+  resources :users do 
+    resources :posts 
+  end 
+
+
 end
