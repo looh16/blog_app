3589dc8e307b568f1b3f62008e3aad1f609421ca
looh16 <langacustodio@gmail.com>
2022-11-10 17:23:24 +0200

    Add form to create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post)
+      .permit(:title, :text)
+  end
 end
