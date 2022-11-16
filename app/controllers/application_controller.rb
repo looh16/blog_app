a0103b27bff780198c73575435da39f7ac7c8b46
looh16 <langacustodio@gmail.com>
2022-11-16 21:04:42 +0200

    Add roles and Update migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  ROLES = %i[admin default].freeze
+
   validates :name, presence: true
 
   def last_posts
